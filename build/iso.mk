b460ff851e4b3becbf50b8aa7ee80e327f79ab1f
Tatsuya Kitagawa <tatsuya.kitagawa@t13a.com>
2020-09-06 16:22:24 +0900

    Support archiso v47

--- build/iso.mk
+++ build/iso.mk
@@ -10,15 +10,10 @@ iso: $(OUT_ISO)
 
 $(OUT_ISO): profile repo
 	$(call PRINT,Building ISO image...)
-	sudo $(OUT_PROFILE_DIR)/build.sh \
-		-N $(ISO_NAME) \
-		-V $(ISO_VERSION) \
-		$(if $(ISO_LABEL),-L "$(ISO_LABEL)") \
-		$(if $(ISO_PUBLISHER),-P "$(ISO_PUBLISHER)") \
-		$(if $(ISO_APPLICATION),-A "$(ISO_APPLICATION)") \
-		$(if $(ISO_INSTALL_DIR),-D "$(ISO_INSTALL_DIR)") \
-		-w $(OUT_ISO_WORK_DIR) \
+	sudo mkarchiso \
 		-o $(OUT_ISO_DIR) \
-		-v
+		-v \
+		-w $(OUT_ISO_WORK_DIR) \
+		$(OUT_PROFILE_DIR)
 	$(call PRINT,Succeessfully built ISO image)
 
